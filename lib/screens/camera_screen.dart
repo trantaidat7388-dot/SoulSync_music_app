720ba5fab1314d30fc5ae276b4572c3ab09eb140
Tấn Đạt Trần <trandatdat7388@gmail.com>
2025-12-31 08:09:42 +0700

    sua camera thành trang trắng

---
+++ lib/screens/camera_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../theme/colors.dart';
+
+class CameraScreen extends StatelessWidget {
+  const CameraScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        color: Colors.white,
+      ),
+    );
+  }
+}
